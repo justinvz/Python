7d511388ac85d138a33bc9141384d9f380ead976
justinvz <justin.vz@live.nl>
2020-09-25 18:57:39 +0200

    Update

--- opdracht2/opdracht2/opdracht2.py
+++ opdracht2/opdracht2/opdracht2.py
@@ -1,39 +1,54 @@
-class place:
-    """een punt in 2d ruimte en orientatie in radiant.
-    attributes: x, y, oriant
-    """
+import math
+
+
+class robot:
+    """Robot"""
+
+
     def __init__(self, x = 0, y = 0, oriant = 0):
         self.x = x
         self.y = y
         self.oriant = oriant
 
-    def position(robot):
-        print(robot.x,robot.y)
-    def orientation(robot):
-        print(robot.oriant)
-    def pose(robot):
-        print(robot.x,robot.y,robot.oriant)
+    def position(self):
+        print(self.x,self.y)
 
-    def foward(robot):
-        robot.y += 1;
+    def orientation(self):
+        print(self.oriant)
 
-    def rotate():
-        pass
+    def pose(self):
+        print(round(self.x,2),round(self.y,2),round(self.oriant,2))
 
-    def change_pose():
-        pass
+    def foward(self, distance):
+        self.y += distance * math.sin(self.oriant)
+        self.x += distance * math.cos(self.oriant)
 
-    def reset_pose():
-        pass
+    def rotate(self, radian):
+        self.oriant += radian
+
+    def change_pose(self,x,y,oriant):
+        self.x = x;
+        self.y = y;
+        self.oriant = oriant;
+
+    def reset_pose(self,x,y,oriant):
+        self.x = x;
+        self.y = y;
+        self.oriant = oriant;
+    
+    def disbetween(self, robot):
+        dx = self.x - robot.x
+        dy = self.y - robot.y
+        return (dx**2 + dy**2)**0.5
 
 
+def distance(robot1, robot2):
+    dx = robot1.x - robot2.x
+    dy = robot1.y - robot2.y
+    return (dx**2+dy**2)**0.5
 
+for i in range(10):
+    roboti = robot()
+    print(i)
 
-         
-robot1 = place()
-robot1.x = 5.0
-robot1.y = 2.0
-robot1.oriant = 3.14
-
-robot1.pose()
 
